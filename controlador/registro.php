4e21e53cd07cfc9f2498ca3aa0daae7acd68b154
Alejo <alejobustamanter@gmail.com>
2024-07-15 15:08:47 -0500

    arregle unos espacios en el mensaje al registrar

--- controlador/registro.php
+++ controlador/registro.php
@@ -9,10 +9,10 @@ $fecha = $_POST["fecha"];
 if (!empty($nombre) && !empty($genero)  && !empty($artistanomb)  && !empty($fecha)  ) {
     $sql = $conexion->query("INSERT INTO conciertos(nombre,genero,artistanomb,fecha) VALUES('$nombre','$genero','$artistanomb' ,'$fecha')");
     if ($sql==1) {
-        echo'<div class="bg-green-400 p-4 text-2xl">Formulario completado correctamente. OK</div>';
+        echo'<div class="bg-green-400 p-2 text-2xl mt-2" >Formulario completado correctamente. OK</div>';
     }
 } else {
-    echo '<div class="bg-red-500 p-4 text-2xl">Por favor, complete todos los campos</div>';
+    echo '<div class="bg-red-500 p-2 mt-2 text-2xl">Por favor, complete todos los campos</div>';
 }
             }
 ?>
