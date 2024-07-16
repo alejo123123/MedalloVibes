be65df2ac1cb5f49200515368be938938b8e4af1
Alejo <alejobustamanter@gmail.com>
2024-07-16 17:21:44 -0500

    argegue  el enlace a la consulta sql

--- controlador/registro.php
+++ controlador/registro.php
@@ -5,11 +5,12 @@ $nombre = $_POST["nombre"];
 $genero = $_POST["genero"];
 $artistanomb = $_POST["artistanomb"];
 $fecha = $_POST["fecha"];
+$enlace = $_POST["enlace"];
 
-if (!empty($nombre) && !empty($genero)  && !empty($artistanomb)  && !empty($fecha)  ) {
-    $sql = $conexion->query("INSERT INTO conciertos(nombre,genero,artistanomb,fecha) VALUES('$nombre','$genero','$artistanomb' ,'$fecha')");
+if (!empty($nombre) && !empty($genero)  && !empty($artistanomb)  && !empty($fecha) && !empty($enlace)  ) {
+    $sql = $conexion->query("INSERT INTO conciertos(nombre,genero,artistanomb,fecha,enlace) VALUES('$nombre','$genero','$artistanomb' ,'$fecha','$enlace')");
     if ($sql==1) {
-        echo'<div class="bg-green-400 p-2 text-2xl mt-2" >Formulario completado correctamente. OK</div>';
+        echo'<div class="bg-green-400 p-2 text-2xl mt-2" >Formulario completado correctamente.</div>';
     }
 } else {
     echo '<div class="bg-red-500 p-2 mt-2 text-2xl">Por favor, complete todos los campos</div>';
