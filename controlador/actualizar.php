f156e8ac395efb1f0b4e5416ebabbe0101530779
Alejo <alejobustamanter@gmail.com>
2024-07-16 17:26:43 -0500

    aregue el enlace

--- controlador/actualizar.php
+++ controlador/actualizar.php
@@ -5,12 +5,14 @@ if (!empty($_POST["registrar"])) {
     $genero = $_POST["genero"];
     $artistanomb = $_POST["artistanomb"];
     $fecha = $_POST["fecha"];
+    $enlace = $_POST["enlace"];
+    
     
 
 
-    if (!empty($nombre) && !empty($genero)  && !empty($artistanomb) && !empty($fecha)) {
+    if (!empty($nombre) && !empty($genero)  && !empty($artistanomb) && !empty($fecha) && !empty($enlace)) {
       
-        $sql = $conexion->query("UPDATE conciertos SET nombre='$nombre', genero='$genero', artistanomb='$artistanomb', fecha='$fecha'  WHERE id_concierto=$id");
+        $sql = $conexion->query("UPDATE conciertos SET nombre='$nombre', genero='$genero', artistanomb='$artistanomb', fecha='$fecha', enlace='$enlace'  WHERE id_concierto=$id");
 
         if ($sql==1) {
             header("location:index.php");
