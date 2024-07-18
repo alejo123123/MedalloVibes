94836422694bd6d0ee813c75806d7e17fc4c8b09
Alejo <alejobustamanter@gmail.com>
2024-07-17 19:04:36 -0500

    inclui el header

--- conciertos.php
+++ conciertos.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
+<?php 
+include 'layouts/header.php';
+?>
+
+
+
+<body >
     
-    <h1 class="text-6xl text-center my-24 ">Nuestros Conciertos</h1>
+    <h1 class="text-6xl text-center my-24 font-kalnia">Nuestros Conciertos</h1>
     <div class="grid md:grid-cols-3 grid-cols-1 md:gap-24 gap-8 mx-6"> 
 
                 <?php 
