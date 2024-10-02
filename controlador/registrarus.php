3f51323e8ee99bf1eef60ce0da4b61ff5b50ac43
Alejo <alejobustamanter@gmail.com>
2024-10-02 15:22:41 -0500

    cambie la locacion donde se redirigen los usuarios luego de registrarse

--- controlador/registrarus.php
+++ controlador/registrarus.php
@@ -9,7 +9,7 @@ if (!empty($us_nombre) && !empty($us_correo)  && !empty($us_contrasena) ) {
     $sql = $conexion->query("INSERT INTO usuarios(us_nombre,us_correo,us_contrasena) VALUES('$us_nombre','$us_correo','$us_contrasena')");
     if ($sql==1) {
         echo'<div class="bg-green-400 p-2 text-2xl">Formulario completado correctamente</div>';
-            header("location:index.php");
+            header("location:conciertos.php");
     }
 } else {
     echo '<div class="bg-red-500 p-2 text-2xl">Por favor, complete todos los campos</div>';
